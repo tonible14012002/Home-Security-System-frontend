1e12b75f85ba6dd6dd9d745996d8ccc85d49c10d
khanhhhhhhh <vthanh0711@gmail.com>
2022-10-22 12:30:14 +0700

    add sidebar

--- src/layouts/DefaultLayout/index.js
+++ src/layouts/DefaultLayout/index.js
@@ -1,9 +1,9 @@
+import Sidebar from "../../components/Sidebar";
+
 const DefaultLayout = ({ children, ...props }) => {
   return (
     <div className="flex">
-      <div className="flex flex-col items-center p-4 gap-10 h-[100vh] w-[100%] max-w-[100px] bg-red-200">
-        Sidebar
-      </div>
+      <Sidebar/>
       <div className="flex-1">Content</div>
     </div>
   );
