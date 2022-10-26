dcd7232e94df045be075290508ee40187acde8d6
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2022-10-26 12:42:21 +0700

    User homepage checked

--- src/layouts/DefaultLayout/index.js
+++ src/layouts/DefaultLayout/index.js
@@ -3,7 +3,7 @@ import Sidebar from '../../components/Sidebar';
 const DefaultLayout = ({ children, ...props }) => {
   return (
     <div className="flex">
-      <div className="flex flex-col items-center p-4 gap-10 h-[100vh] w-[100%] max-w-[100px] bg-red-200">
+      <div className="flex flex-col items-center p-4 gap-10 h-[100vh] w-[100%] max-w-[100px] border-r-2">
         <Sidebar />
       </div>
       <div className="flex-1">{children}</div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,35 +1,35 @@
-import Button from "../../components/Button"
+import Button from '../../components/Button';
 import InputField from '../../components/InputField';
-import {
-    UserCircleIcon
-
-} from '@heroicons/react/24/outline'
+import { UserCircleIcon } from '@heroicons/react/24/outline';
 const Home = () => {
+  return (
+    <div>
+      <div className="text-black px-10 tablet:px-[30px] desktop:px-[150px] pt-[40px] text-2xl tracking-wide">
+        Welcome, <span className="font-bold">User!</span>
+      </div>
+      <div className="flex flex-col items-center justify-center leading-normal">
+        <p className="font-bold text-4xl mb-[50px] mt-[70px] text-center">
+          My information
+        </p>
 
-    return (
-        <div className ='text-black mx-[150px] mt-[40px] text-xl' > 
-        Welcome, <span className = 'font-bold'>User!</span>   
-           
-            <div className='flex flex-col items-center justify-center ml-[250px] mr-[250px] mt-[70px] leading-normal'> 
-            <p className='font-bold text-2xl mb-[20px]'>My information</p>
+        <form>
+          <div className="flex flex-col gap-5 max-w-[350px] tablet:min-w-[500px] mx-auto">
             <InputField field="Full Name" type="text" />
             <InputField field="Phone No" type="text" />
             <InputField field="Address" type="text" />
             <InputField field="Date of Birth" type="text" />
-            <InputField field="Date of Birth" type="text" />
-            
-            
-            <div class="flex w-full mt-[15px] ">
-            <UserCircleIcon className=" w-20 h-20" />
-            <div> 
-            <Button primary text="Change Avatar" />
-            </div>
+            <div class="flex items-center w-full">
+              <UserCircleIcon className=" w-20 h-20" />
+              <div>
+                <Button text="Change Avatar" />
+              </div>
             </div>
             <Button primary text="Update" />
-            </div> 
-                
-        </div>
-    )
-}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
