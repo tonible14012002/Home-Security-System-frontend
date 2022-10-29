82264fb1b93cd84fd7821c464f5b452a05fb4d4f
khanhhhhhhh <vthanh0711@gmail.com>
2022-10-29 11:59:46 +0700

    "EditProfile"

--- src/components/Editprofile/index.js
+++ src/components/Editprofile/index.js
@@ -1,39 +1,41 @@
 import Button from '../../components/Button';
 import InputField from '../../components/InputField';
-import { UserCircleIcon } from '@heroicons/react/24/outline';
+import { UserCircleIcon, CheckCircleIcon } from '@heroicons/react/24/outline';
 import ModalLayout from '../../layouts/ModalLayout';
-const Home = () => {
+const EditProfile = () => {
   return (
     <div>
       <div className="text-black text-center tablet:text-left tablet:px-[30px] desktop:px-[150px] pt-[40px] text-2xl tracking-wide">
         Welcome, <span className="font-bold">User!</span>
       </div>
-      <div className="flex flex-col items-center justify-center leading-normal">
-        <p className="font-bold text-4xl mb-[50px] mt-[70px] text-center">
-          My information
-        </p>
-
-        <form>
-          <div className="flex flex-col gap-5 min-w-[350px] max-w-[400px] w-[100%] tablet:min-w-[500px] mx-auto">
-            <InputField field="Full Name" type="text" />
-            <InputField field="Phone No" type="text" />
-            <InputField field="Address" type="text" />
-            <InputField field="Date of Birth" type="text" />
-            <div class="flex items-center w-full">
-              <UserCircleIcon className=" w-20 h-20" />
-              <div>
-                <Button text="Change Avatar" />
-              </div>
-            </div>
-            <Button primary text="Update" />
-          </div>
-        </form>
-      </div>
       <ModalLayout>
-        <div className="w-5 h-5 bg-white"></div> {/* Your component*/}
+        <div className="w-250 h-250 bg-white"></div> {
+              <div className="flex flex-col items-center justify-center leading-normal">
+              <p className="font-bold text-4xl mb-[50px] mt-[70px] text-center">
+                User information
+              </p>
+      
+              <form>
+                <div className="flex flex-col gap-5 min-w-[350px] max-w-[400px] w-[100%] tablet:min-w-[500px] mx-auto">
+                  <InputField field="Full Name" type="text" />
+                  <InputField field="Phone No" type="text" />
+                  <InputField field="Address" type="text" />
+                  <InputField field="Date of Birth" type="text" />
+                  <div class="flex items-center w-full">
+                    <UserCircleIcon className=" w-20 h-20" />
+                    <div>
+                      <p>Auto open door</p>
+                      <CheckCircleIcon className=" w-20 h-20" />
+                    </div>
+                  </div>
+                  <Button primary text="Update" />
+                </div>
+              </form>
+            </div>
+        }
       </ModalLayout>
     </div>
   );
 };
 
-export default Home;
+export default EditProfile;
