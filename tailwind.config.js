024a2cbb5d68a15135a5657ed5dee5cf18a5a762
MinhLeTuan246 <116412241+MinhLeTuan246@users.noreply.github.com>
2022-10-29 19:47:42 +0700

    Admin page

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,20 @@ module.exports = {
       desktop: '1280px',
       // => @media (min-width: 1280px) { ... }
     },
+    backgroundImage: {
+      'pack-train': "url('../public/packTrain.jpg')",
+    },
     extend: {
+      backgroundImage: {
+        'pack-train': "url('/public/pack-train.jpg')",
+      },
       colors: {
         mainCream: '#F8EDE3',
         mainPurple: '#7D6E83',
         mainRed: '#F96666',
         mainBlue: '#7895B2',
         mainBrown: '#D0B8A8',
+        mainGreen: '#48AD0E',
       },
     },
   },
